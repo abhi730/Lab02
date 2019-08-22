38472067c04d4c01d942d5d1af904e803471861a
abhi <abhishek.bhardwaj_cs18@gla.ac.in>
2019-08-22 14:59:09 +0530

    add file Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * @author abhi
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] arg) {
+        System.out.println("Enter your input String ");
+        Scanner a = new Scanner(System.in);
+        String name = a.nextLine();
+        System.out.println("welcome, " + name + "!");
+
+
+    }
+}
