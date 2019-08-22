7131c13d59c84824be14de1c291933386d1704a2
abhi <abhishek.bhardwaj_cs18@gla.ac.in>
2019-08-22 15:13:19 +0530

    create object of Scanner class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author abhi
  * @version 1.0
@@ -7,7 +9,9 @@ package main;
 public class Main {
     public static void main(String[] args) {
         system.out.println("Enter your input String ");
+        Scanner scanner = new Scanner(System.in);
         String userName = "";
+        scanner.close();
 
     }
 }
